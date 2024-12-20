b8106ebdb069fd2ff99d98976f61fe230435c985
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2024-12-20 11:32:55 +0700

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "https://the-handmade-hub-api.vercel.app/api";
-const BASE_URL = "https://the-handmade-hub-api.vercel.app/api";
+const BASE_URL = "....";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
